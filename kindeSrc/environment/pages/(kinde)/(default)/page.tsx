a1a8f05186ebd3d151575782057cfc9141139ada
Peter Phanouvong <peterphanouvong@gmail.com>
2025-01-06 15:29:17 +1100

    try

--- kindeSrc/environment/pages/(kinde)/(default)/page.tsx
+++ kindeSrc/environment/pages/(kinde)/(default)/page.tsx
@@ -2,7 +2,12 @@
 
 import React from "react";
 
-import { getKindeRequiredCSS, getKindeRequiredJS } from "@kinde/infrastructure";
+import {
+  getKindeRequiredCSS,
+  getKindeRequiredJS,
+  getKindeCSRF,
+  getKindeWidget,
+} from "@kinde/infrastructure";
 import { renderToString } from "react-dom/server";
 
 export const pageSettings = {
@@ -14,8 +19,25 @@ export const pageSettings = {
 const Layout = async ({ request }) => {
   return (
     <html lang={request.locale.lang}>
-      <head>meta</head>
-      <body>sup</body>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="robots" content="noindex" />
+        <meta name="csrf-token" content={getKindeCSRF()} />
+        <title>{kinde.localization.get("page_title")}</title>
+        {getKindeRequiredCSS()}
+        {getKindeRequiredJS()}
+      </head>
+      <body>
+        <div id="root" data-roast-root="/admin">
+          <header>Company name</header>
+          <main>
+            <h2>{kinde.localization.get("heading")}</h2>
+            <p>{kinde.localization.get("description")}</p>
+            <div>{getKindeWidget()}</div>
+          </main>
+        </div>
+      </body>
     </html>
   );
 };

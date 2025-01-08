ba4e78c8115d470461e6f496de9325f825163d8c
Peter Phanouvong <peterphanouvong@gmail.com>
2025-01-09 09:21:53 +1100

    new style

--- kinde/environment/pages/(kinde)/(default)/page.tsx
+++ kinde/environment/pages/(kinde)/(default)/page.tsx
@@ -25,47 +25,36 @@ const Layout = async ({ request, context }: KindePageEvent) => {
         {getKindeRequiredJS()}
         <style>{`
         :root {
-  --kinde-button-primary-background-color: #0063e0;
-  --kinde-button-primary-color: white;
-  --kinde-button-font-weight: 600;
-  --kinde-designer-control-select-text-border-radius: 12px;
-  --kinde-button-border-radius: 24px;
-  --
-}
+          --kinde-button-primary-background-color: #0063e0;
+          --kinde-button-primary-color: white;
+          --kinde-button-font-weight: 600;
+          --kinde-designer-control-select-text-border-radius: 12px;
+          --kinde-button-border-radius: 24px;
+        }
 
-.kinde-control-label {
-  display: none;
-}
+        .kinde-control-label {
+          display: none;
+        }
 
-* {
-  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
-}
+        * {
+          font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+        }
 
-header {
-  text-align: center;
-}
+        header {
+          text-align: center;
+        }
 
-.links {
-  display: flex;
-  justify-content: center;
-  gap: 12px;
-  font-size: 12px;
+        .links {
+          display: flex;
+          justify-content: center;
+          gap: 12px;
+          font-size: 12px;
+        }
 
-
-}
-
-.links a{
-  text-decoration: none;
-  color: #777;
-}
-
-
-.kinde-form {
-
-  & > *:not(:nth-last-child(2)) {
-    margin-block-end: 1rem;
-  }
-}
+        .links a{
+          text-decoration: none;
+          color: #777;
+        }
 
         `}</style>
       </head>

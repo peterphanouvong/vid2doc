973b8c18581810f909787ff5281aa506aefd05d8
Peter Phanouvong <peterphanouvong@gmail.com>
2025-01-08 11:55:39 +1100

    new setup

--- kindeSrc/environment/pages/(kinde)/(default)/page.tsx
+++ kindeSrc/environment/pages/(kinde)/(default)/page.tsx
@@ -2,29 +2,17 @@
 
 import React from "react";
 
-const getKindeWidget = () => "@cd65da2987c740d58961024aa4a27194@";
-
-const getKindeNonce = () => "@43dffdf2c22f40e9981303cb383f6fac@";
-
-const getKindeRequiredCSS = () => "@ce0ef44d50f6408985f00c04a85d8430@";
-
-const getKindeRequiredJS = () => "@8103c7ff23fe49edb9b0537d2927e74e@";
-
-const getKindeCSRF = () => "@0c654432670c4d0292c3a0bc3c533247@";
-
-const getKindeSignUpUrl = () => "@b1d3a51558e64036ad072b56ebae37f5@";
-
-const getKindeSignInUrl = () => "@847681e125384709836f921deb311104@";
+import {
+  getKindeRequiredCSS,
+  getKindeRequiredJS,
+  getKindeCSRF,
+  getKindeWidget,
+} from "@kinde/infrastructure";
 
 import { renderToString } from "react-dom/server.browser";
+import { KindePageEvent } from "@/lib/utils";
 
-export const pageSettings = {
-  bindings: {
-    "kinde.localization": {},
-  },
-};
-
-const Layout = async ({ request }) => {
+const Layout = async ({ request, context }: KindePageEvent) => {
   return (
     <html lang={request.locale.lang}>
       <head>
@@ -32,25 +20,13 @@ const Layout = async ({ request }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="robots" content="noindex" />
         <meta name="csrf-token" content={getKindeCSRF()} />
-        <title>{kinde.localization.get("page_title")}</title>
+        <title>{context.widget.content.page_title}</title>
         {getKindeRequiredCSS()}
         {getKindeRequiredJS()}
-        <style nonce={getKindeNonce()}>
-          {`
-            .c-header {
-              text-align: center;
-            }
-          `}
-        </style>
       </head>
       <body>
         <div id="root" data-roast-root="/admin">
-          <header className="c-header">
-            <div>English (UK)</div>
-          </header>
           <main>
-            <h2>{kinde.localization.get("heading")}</h2>
-            <p>{kinde.localization.get("description")}</p>
             <div>{getKindeWidget()}</div>
           </main>
         </div>
@@ -59,7 +35,7 @@ const Layout = async ({ request }) => {
   );
 };
 
-export default async function Page(event) {
+export default async function Page(event: KindePageEvent) {
   const page = await Layout({ ...event });
   return renderToString(page);
 }

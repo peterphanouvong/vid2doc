e88b53d5f95f54f549849c7cfc6683eba65f4d33
Peter Phanouvong <peterphanouvong@gmail.com>
2025-02-04 10:54:57 +1100

    dark mode variables

--- kindeSrc/environment/pages/(kinde)/(default)/page.tsx
+++ kindeSrc/environment/pages/(kinde)/(default)/page.tsx
@@ -10,60 +10,6 @@ import React from "react";
 import { renderToString } from "react-dom/server.browser";
 import Layout from "../../layout";
 
-const styles: {
-  container: React.CSSProperties;
-  sidePanel: React.CSSProperties;
-  loginForm: React.CSSProperties;
-  heading: React.CSSProperties;
-  description: React.CSSProperties;
-  logoWrapper: React.CSSProperties;
-  logo: React.CSSProperties;
-} = {
-  container: {
-    display: "flex",
-    height: "100vh",
-  },
-  sidePanel: {
-    borderTopLeftRadius: "2rem",
-    borderBottomLeftRadius: "2rem",
-    borderBottomRightRadius: "0.5rem",
-    borderTopRightRadius: "0.5rem",
-    backgroundColor: "#f5f5f5",
-    borderColor: "#ccc",
-    borderStyle: "solid",
-    borderWidth: "1px",
-    flex: 1,
-    margin: "0.5rem",
-    maxWidth: "1024px",
-  },
-  loginForm: {
-    minWidth: "400px",
-    margin: "0 auto",
-    minInlineSize: "2rem",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-  },
-  heading: {
-    fontWeight: "600",
-    fontSize: "1.5rem",
-  },
-  description: {
-    fontSize: "14px",
-    marginBottom: "1.5rem",
-    color: "#555",
-  },
-  logoWrapper: {
-    display: "flex",
-    justifyContent: "center",
-    marginBottom: "1rem",
-  },
-  logo: {
-    width: "44px",
-    height: "44px",
-  },
-};
-
 const DefaultPage: React.FC<KindePageEvent> = ({ context, request }) => {
   return (
     <Layout context={context} request={request}>
